71c7c808a7e2632842ae90befa97b477dc0bb73e
Elijah Semyonov <elijah.semenov@gmail.com>
2024-08-06 21:49:00 +0200

    Port Seb code

--- Sources/SwiftOffsetAllocator/SwiftOffsetAllocator.swift
+++ Sources/SwiftOffsetAllocator/SwiftOffsetAllocator.swift
@@ -1,2 +1,388 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+#if USE_16_BIT_NODE_INDICES
+    typealias NodeIndex = UInt16
+#else
+    typealias NodeIndex = UInt32
+#endif
+
+let NUM_TOP_BINS: UInt32 = 32
+let BINS_PER_LEAF: UInt32 = 8
+let TOP_BINS_INDEX_SHIFT: UInt32 = 3
+let LEAF_BINS_INDEX_MASK: UInt32 = 0x7
+let NUM_LEAF_BINS = NUM_TOP_BINS * BINS_PER_LEAF
+
+struct Allocation {
+    static let NO_SPACE: UInt32 = 0xffffffff
+    
+    var offset: UInt32 = NO_SPACE
+    var metadata: NodeIndex = NO_SPACE // internal: node index
+}
+
+struct StorageReport {
+    var totalFreeSpace: UInt32
+    var largestFreeRegion: UInt32
+}
+
+struct StorageReportFull {
+    struct Region {
+        var size: UInt32
+        var count: UInt32
+    }
+    
+    var freeRegions: [Region] = Array(repeating: Region(size: 0, count: 0), count: Int(NUM_LEAF_BINS))
+}
+
+class Allocator {
+    struct Node {
+        static let unused: NodeIndex = 0xffffffff
+        
+        var dataOffset: UInt32 = 0
+        var dataSize: UInt32 = 0
+        var binListPrev: NodeIndex = Node.unused
+        var binListNext: NodeIndex = Node.unused
+        var neighborPrev: NodeIndex = Node.unused
+        var neighborNext: NodeIndex = Node.unused
+        var used: Bool = false // TODO: Merge as bit flag
+    }
+    
+    private var m_size: UInt32
+    private var m_maxAllocs: UInt32
+    private var m_freeStorage: UInt32 = 0
+
+    private var m_usedBinsTop: UInt32 = 0
+    private var m_usedBins: [UInt8] = Array(repeating: 0, count: Int(NUM_TOP_BINS))
+    private var m_binIndices: [NodeIndex] = Array(repeating: Node.unused, count: Int(NUM_LEAF_BINS))
+                
+    private var m_nodes: UnsafeMutablePointer<Node>
+    private var m_freeNodes: [NodeIndex] = []
+    private var m_freeOffset: UInt32 = 0
+
+    init(size: UInt32, maxAllocs: UInt32 = 128 * 1024) {
+        self.m_size = size
+        self.m_maxAllocs = maxAllocs
+        
+        m_nodes = .allocate(capacity: Int(m_maxAllocs))
+        m_nodes.initialize(repeating: Node(), count: Int(m_maxAllocs))
+        
+        m_freeStorage = 0
+        m_usedBinsTop = 0
+        m_freeOffset = m_maxAllocs - 1
+
+        for i in 0..<Int(NUM_TOP_BINS) {
+            m_usedBins[i] = 0
+        }
+        
+        for i in 0..<Int(NUM_LEAF_BINS) {
+            m_binIndices[i] = Node.unused
+        }
+        
+        //m_nodes = [Node](repeating: Node(), count: Int(m_maxAllocs))
+        m_freeNodes = [NodeIndex](repeating: 0, count: Int(m_maxAllocs))
+        
+        for i in 0..<Int(m_maxAllocs) {
+            m_freeNodes[i] = m_maxAllocs - UInt32(i) - 1
+        }
+        
+        _ = insertNodeIntoBin(size: m_size, dataOffset: 0)
+    }
+    
+    func allocate(size: UInt32) -> Allocation {
+        if m_freeOffset == 0 {
+            return Allocation(offset: Allocation.NO_SPACE, metadata: Allocation.NO_SPACE)
+        }
+        
+        let minBinIndex = SmallFloat.uintToFloatRoundUp(size)
+        let minTopBinIndex = minBinIndex >> TOP_BINS_INDEX_SHIFT
+        let minLeafBinIndex = minBinIndex & LEAF_BINS_INDEX_MASK
+                
+        var topBinIndex = minTopBinIndex
+        var leafBinIndex: UInt32 = Allocation.NO_SPACE
+        
+        if (m_usedBinsTop & (1 << topBinIndex)) != 0 {
+            leafBinIndex = findLowestSetBitAfter(bitMask: UInt32(m_usedBins[Int(topBinIndex)]), startBitIndex: minLeafBinIndex)
+        }
+    
+        if leafBinIndex == Allocation.NO_SPACE {
+            topBinIndex = findLowestSetBitAfter(bitMask: m_usedBinsTop, startBitIndex: minTopBinIndex + 1)
+            
+            if topBinIndex == Allocation.NO_SPACE {
+                return Allocation(offset: Allocation.NO_SPACE, metadata: Allocation.NO_SPACE)
+            }
+
+            leafBinIndex = tzcnt_nonzero(UInt32(m_usedBins[Int(topBinIndex)]))
+        }
+        
+        let binIndex = (topBinIndex << TOP_BINS_INDEX_SHIFT) | leafBinIndex
+        let nodeIndex = m_binIndices[Int(binIndex)]
+        
+        let pNode = m_nodes.advanced(by: Int(nodeIndex))
+        
+        let nodeTotalSize = pNode.pointee.dataSize
+        pNode.pointee.dataSize = size
+        pNode.pointee.used = true
+        m_binIndices[Int(binIndex)] = pNode.pointee.binListNext
+        if pNode.pointee.binListNext != Node.unused {
+            m_nodes[Int(pNode.pointee.binListNext)].binListPrev = Node.unused
+        }
+        m_freeStorage -= nodeTotalSize
+        
+        if m_binIndices[Int(binIndex)] == Node.unused {
+            m_usedBins[Int(topBinIndex)] &= ~(1 << leafBinIndex)
+            if m_usedBins[Int(topBinIndex)] == 0 {
+                m_usedBinsTop &= ~(1 << topBinIndex)
+            }
+        }
+        
+        let reminderSize = nodeTotalSize - size
+        if reminderSize > 0 {
+            let newNodeIndex = insertNodeIntoBin(size: reminderSize, dataOffset: pNode.pointee.dataOffset + size)
+            
+            if pNode.pointee.neighborNext != Node.unused {
+                m_nodes[Int(pNode.pointee.neighborNext)].neighborPrev = newNodeIndex
+            }
+            m_nodes[Int(newNodeIndex)].neighborPrev = nodeIndex
+            m_nodes[Int(newNodeIndex)].neighborNext = pNode.pointee.neighborNext
+            pNode.pointee.neighborNext = newNodeIndex
+        }
+                
+        return Allocation(offset: pNode.pointee.dataOffset, metadata: nodeIndex)
+    }
+    
+    func free(allocation: Allocation) {
+        assert(allocation.metadata != Allocation.NO_SPACE)
+        
+        let nodeIndex = allocation.metadata
+        
+        let pNode = m_nodes.advanced(by: Int(nodeIndex))
+        assert(pNode.pointee.used)
+        
+        var offset = pNode.pointee.dataOffset
+        var size = pNode.pointee.dataSize
+        
+        
+        if (pNode.pointee.neighborPrev != Node.unused) {
+            print("m_nodes[\(pNode.pointee.neighborPrev)].used = \(m_nodes[Int(pNode.pointee.neighborPrev)].used)")
+        }
+              
+        if (pNode.pointee.neighborPrev != Node.unused) && (m_nodes[Int(pNode.pointee.neighborPrev)].used == false) {
+            let prevNode = m_nodes[Int(pNode.pointee.neighborPrev)]
+            offset = prevNode.dataOffset
+            size += prevNode.dataSize
+            
+            removeNodeFromBin(nodeIndex: pNode.pointee.neighborPrev)
+            
+            assert(prevNode.neighborNext == nodeIndex)
+            pNode.pointee.neighborPrev = prevNode.neighborPrev
+        }
+        
+        if (pNode.pointee.neighborNext != Node.unused) && (m_nodes[Int(pNode.pointee.neighborNext)].used == false) {            
+            let nextNode = m_nodes[Int(pNode.pointee.neighborNext)]
+            size += nextNode.dataSize
+            
+            removeNodeFromBin(nodeIndex: pNode.pointee.neighborNext)
+            
+            assert(nextNode.neighborPrev == nodeIndex)
+            pNode.pointee.neighborNext = nextNode.neighborNext
+        }
+
+        let neighborNext = pNode.pointee.neighborNext
+        let neighborPrev = pNode.pointee.neighborPrev
+        
+        m_freeOffset += 1
+        m_freeNodes[Int(m_freeOffset)] = nodeIndex
+
+        let combinedNodeIndex = insertNodeIntoBin(size: size, dataOffset: offset)
+
+        if neighborNext != Node.unused {
+            m_nodes[Int(combinedNodeIndex)].neighborNext = neighborNext
+            m_nodes[Int(neighborNext)].neighborPrev = combinedNodeIndex
+        }
+        if neighborPrev != Node.unused {
+            m_nodes[Int(combinedNodeIndex)].neighborPrev = neighborPrev
+            m_nodes[Int(neighborPrev)].neighborNext = combinedNodeIndex
+        }
+    }
+
+    private func insertNodeIntoBin(size: UInt32, dataOffset: UInt32) -> NodeIndex {
+        let binIndex = SmallFloat.uintToFloatRoundDown(size)
+        let topBinIndex = binIndex >> TOP_BINS_INDEX_SHIFT
+        let leafBinIndex = binIndex & LEAF_BINS_INDEX_MASK
+        
+        if m_binIndices[Int(binIndex)] == Node.unused {
+            m_usedBins[Int(topBinIndex)] |= 1 << leafBinIndex
+            m_usedBinsTop |= 1 << topBinIndex
+        }
+        
+        let topNodeIndex = m_binIndices[Int(binIndex)]
+        let nodeIndex = m_freeNodes[Int(m_freeOffset)]
+        m_freeOffset -= 1
+                
+        m_nodes[Int(nodeIndex)] = Node(dataOffset: dataOffset, dataSize: size, binListNext: topNodeIndex)
+        
+        if topNodeIndex != Node.unused {
+            m_nodes[Int(topNodeIndex)].binListPrev = nodeIndex
+        }
+        m_binIndices[Int(binIndex)] = nodeIndex
+        
+        m_freeStorage += size
+        
+        return nodeIndex
+    }
+    
+    private func removeNodeFromBin(nodeIndex: NodeIndex) {
+        let node = m_nodes[Int(nodeIndex)]
+        
+        if node.binListPrev != Node.unused {
+            m_nodes[Int(node.binListPrev)].binListNext = node.binListNext
+            if node.binListNext != Node.unused {
+                m_nodes[Int(node.binListNext)].binListPrev = node.binListPrev
+            }
+        } else {
+            let binIndex = SmallFloat.uintToFloatRoundDown(node.dataSize)
+            let topBinIndex = binIndex >> TOP_BINS_INDEX_SHIFT
+            let leafBinIndex = binIndex & LEAF_BINS_INDEX_MASK
+            
+            m_binIndices[Int(binIndex)] = node.binListNext
+            if node.binListNext != Node.unused {
+                m_nodes[Int(node.binListNext)].binListPrev = Node.unused
+            }
+
+            if m_binIndices[Int(binIndex)] == Node.unused {
+                m_usedBins[Int(topBinIndex)] &= ~(1 << leafBinIndex)
+                if m_usedBins[Int(topBinIndex)] == 0 {
+                    m_usedBinsTop &= ~(1 << topBinIndex)
+                }
+            }
+        }
+        
+        m_freeOffset += 1
+        m_freeNodes[Int(m_freeOffset)] = nodeIndex
+
+        m_freeStorage -= node.dataSize
+    }
+
+    func allocationSize(allocation: Allocation) -> UInt32 {
+        if allocation.metadata == Allocation.NO_SPACE { return 0 }
+        
+        return m_nodes[Int(allocation.metadata)].dataSize
+    }
+
+    func storageReport() -> StorageReport {
+        var largestFreeRegion: UInt32 = 0
+        var freeStorage: UInt32 = 0
+        
+        if m_freeOffset > 0 {
+            freeStorage = m_freeStorage
+            if m_usedBinsTop != 0 {
+                let topBinIndex = 31 - lzcnt_nonzero(m_usedBinsTop)
+                let leafBinIndex = 31 - lzcnt_nonzero(UInt32(m_usedBins[Int(topBinIndex)]))
+                largestFreeRegion = SmallFloat.floatToUint((topBinIndex << TOP_BINS_INDEX_SHIFT) | leafBinIndex)
+                assert(freeStorage >= largestFreeRegion)
+            }
+        }
+
+        return StorageReport(totalFreeSpace: freeStorage, largestFreeRegion: largestFreeRegion)
+    }
+
+    func storageReportFull() -> StorageReportFull {
+        var report = StorageReportFull()
+        for i in 0..<Int(NUM_LEAF_BINS) {
+            var count: UInt32 = 0
+            var nodeIndex = m_binIndices[i]
+            while nodeIndex != Node.unused {
+                nodeIndex = m_nodes[Int(nodeIndex)].binListNext
+                count += 1
+            }
+            report.freeRegions[i] = StorageReportFull.Region(size: SmallFloat.floatToUint(UInt32(i)), count: count)
+        }
+        return report
+    }
+}
+
+func lzcnt_nonzero(_ v: UInt32) -> UInt32 {
+#if os(Windows)
+    var retVal: UInt32 = 0
+    _BitScanReverse(&retVal, v)
+    return 31 - retVal
+#else
+    return UInt32(v.leadingZeroBitCount)
+#endif
+}
+
+func tzcnt_nonzero(_ v: UInt32) -> UInt32 {
+#if os(Windows)
+    var retVal: UInt32 = 0
+    _BitScanForward(&retVal, v)
+    return retVal
+#else
+    return UInt32(v.trailingZeroBitCount)
+#endif
+}
+
+enum SmallFloat {
+    static let MANTISSA_BITS: UInt32 = 3
+    static let MANTISSA_VALUE: UInt32 = 1 << MANTISSA_BITS
+    static let MANTISSA_MASK: UInt32 = MANTISSA_VALUE - 1
+    
+    static func uintToFloatRoundUp(_ size: UInt32) -> UInt32 {
+        var exp: UInt32 = 0
+        var mantissa: UInt32 = 0
+        
+        if size < MANTISSA_VALUE {
+            mantissa = size
+        } else {
+            let leadingZeros = lzcnt_nonzero(size)
+            let highestSetBit = 31 - leadingZeros
+            
+            let mantissaStartBit = highestSetBit - MANTISSA_BITS
+            exp = mantissaStartBit + 1
+            mantissa = (size >> mantissaStartBit) & MANTISSA_MASK
+            
+            let lowBitsMask = UInt32((1 << mantissaStartBit) - 1)
+            
+            if (size & lowBitsMask) != 0 {
+                mantissa += 1
+            }
+        }
+        
+        return (exp << MANTISSA_BITS) + mantissa
+    }
+
+    static func uintToFloatRoundDown(_ size: UInt32) -> UInt32 {
+        var exp: UInt32 = 0
+        var mantissa: UInt32 = 0
+        
+        if size < MANTISSA_VALUE {
+            mantissa = size
+        } else {
+            let leadingZeros = lzcnt_nonzero(size)
+            let highestSetBit = 31 - leadingZeros
+            
+            let mantissaStartBit = highestSetBit - MANTISSA_BITS
+            exp = mantissaStartBit + 1
+            mantissa = (size >> mantissaStartBit) & MANTISSA_MASK
+        }
+        
+        return (exp << MANTISSA_BITS) | mantissa
+    }
+
+    static func floatToUint(_ floatValue: UInt32) -> UInt32 {
+        let exponent = floatValue >> MANTISSA_BITS
+        let mantissa = floatValue & MANTISSA_MASK
+        if exponent == 0 {
+            return mantissa
+        } else {
+            return (mantissa | MANTISSA_VALUE) << (exponent - 1)
+        }
+    }
+}
+
+func findLowestSetBitAfter(bitMask: UInt32, startBitIndex: UInt32) -> UInt32 {
+    let maskBeforeStartIndex = UInt32((1 << startBitIndex) - 1)
+    let maskAfterStartIndex = ~maskBeforeStartIndex
+    let bitsAfter = bitMask & maskAfterStartIndex
+    if bitsAfter == 0 { return Allocation.NO_SPACE }
+    return tzcnt_nonzero(bitsAfter)
+}
--- Tests/SwiftOffsetAllocatorTests/SwiftOffsetAllocatorTests.swift
+++ Tests/SwiftOffsetAllocatorTests/SwiftOffsetAllocatorTests.swift
@@ -1,12 +1,214 @@
 import XCTest
 @testable import SwiftOffsetAllocator
 
-final class SwiftOffsetAllocatorTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+class SmallFloatTests: XCTestCase {
+    
+    func testUintToFloat() {
+        // Denorms, exp=1 and exp=2 + mantissa = 0 are all precise.
+        // NOTE: Assuming 8 value (3 bit) mantissa.
+        // If this test fails, please change this assumption!
+        let preciseNumberCount: UInt32 = 17
+        for i in 0..<preciseNumberCount {
+            let roundUp = SmallFloat.uintToFloatRoundUp(i)
+            let roundDown = SmallFloat.uintToFloatRoundDown(i)
+            XCTAssertEqual(i, roundUp)
+            XCTAssertEqual(i, roundDown)
+        }
+        
+        // Test some random picked numbers
+        let testData: [(number: UInt32, up: UInt32, down: UInt32)] = [
+            (number: 17, up: 17, down: 16),
+            (number: 118, up: 39, down: 38),
+            (number: 1024, up: 64, down: 64),
+            (number: 65536, up: 112, down: 112),
+            (number: 529445, up: 137, down: 136),
+            (number: 1048575, up: 144, down: 143),
+        ]
+        
+        for v in testData {
+            let roundUp = SmallFloat.uintToFloatRoundUp(v.number)
+            let roundDown = SmallFloat.uintToFloatRoundDown(v.number)
+            XCTAssertEqual(roundUp, v.up)
+            XCTAssertEqual(roundDown, v.down)
+        }
+    }
+    
+    func testFloatToUint() {
+        // Denorms, exp=1 and exp=2 + mantissa = 0 are all precise.
+        // NOTE: Assuming 8 value (3 bit) mantissa.
+        // If this test fails, please change this assumption!
+        let preciseNumberCount: UInt32 = 17
+        for i in 0..<preciseNumberCount {
+            let v = SmallFloat.floatToUint(i)
+            XCTAssertEqual(i, v)
+        }
+        
+        // Test that float->uint->float conversion is precise for all numbers
+        // NOTE: Test values < 240. 240->4G = overflows 32 bit integer
+        for i in 0..<UInt32(240){
+            let v = SmallFloat.floatToUint(UInt32(i))
+            let roundUp = SmallFloat.uintToFloatRoundUp(v)
+            let roundDown = SmallFloat.uintToFloatRoundDown(v)
+            XCTAssertEqual(i, roundUp)
+            XCTAssertEqual(i, roundDown)
+        }
+    }
+}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+class OffsetAllocatorTests: XCTestCase {
+    
+    func testBasic() {
+        let allocator = Allocator(size: 1024 * 1024 * 256)
+        let a = allocator.allocate(size: 1337)
+        let offset = a.offset
+        XCTAssertEqual(offset, 0)
+        allocator.free(allocation: a)
+    }
+    
+    func testAllocate() {
+        let allocator = Allocator(size: 1024 * 1024 * 256)
+        
+        func testSimple() {
+            let a = allocator.allocate(size: 0)
+            XCTAssertEqual(a.offset, 0)
+            
+            let b = allocator.allocate(size: 1)
+            XCTAssertEqual(b.offset, 0)
+            
+            let c = allocator.allocate(size: 123)
+            XCTAssertEqual(c.offset, 1)
+            
+            let d = allocator.allocate(size: 1234)
+            XCTAssertEqual(d.offset, 124)
+            
+            allocator.free(allocation: a)
+            allocator.free(allocation: b)
+            allocator.free(allocation: c)
+            allocator.free(allocation: d)
+            
+            let validateAll = allocator.allocate(size: 1024 * 1024 * 256)
+            XCTAssertEqual(validateAll.offset, 0)
+            allocator.free(allocation: validateAll)
+        }
+        
+        func testMergeTrivial() {
+            let a = allocator.allocate(size: 1337)
+            XCTAssertEqual(a.offset, 0)
+            allocator.free(allocation: a)
+            
+            let b = allocator.allocate(size: 1337)
+            XCTAssertEqual(b.offset, 0)
+            allocator.free(allocation: b)
+            
+            let validateAll = allocator.allocate(size: 1024 * 1024 * 256)
+            XCTAssertEqual(validateAll.offset, 0)
+            allocator.free(allocation: validateAll)
+        }
+        
+        func testReuseTrivial() {
+            let a = allocator.allocate(size: 1024)
+            XCTAssertEqual(a.offset, 0)
+            
+            let b = allocator.allocate(size: 3456)
+            XCTAssertEqual(b.offset, 1024)
+            
+            allocator.free(allocation: a)
+            
+            let c = allocator.allocate(size: 1024)
+            XCTAssertEqual(c.offset, 0)
+            
+            allocator.free(allocation: c)
+            allocator.free(allocation: b)
+            
+            let validateAll = allocator.allocate(size: 1024 * 1024 * 256)
+            XCTAssertEqual(validateAll.offset, 0)
+            allocator.free(allocation: validateAll)
+        }
+        
+        func testReuseComplex() {
+            let a = allocator.allocate(size: 1024)
+            XCTAssertEqual(a.offset, 0)
+            
+            let b = allocator.allocate(size: 3456)
+            XCTAssertEqual(b.offset, 1024)
+            
+            allocator.free(allocation: a)
+            
+            let c = allocator.allocate(size: 2345)
+            XCTAssertEqual(c.offset, 1024 + 3456)
+            
+            let d = allocator.allocate(size: 456)
+            XCTAssertEqual(d.offset, 0)
+            
+            let e = allocator.allocate(size: 512)
+            XCTAssertEqual(e.offset, 456)
+                        
+            let report = allocator.storageReport()
+            let expected: UInt32 = 1024 * 1024 * 256 - 3456 - 2345 - 456 - 512
+            XCTAssertEqual(report.totalFreeSpace, expected)
+            XCTAssertNotEqual(report.largestFreeRegion, report.totalFreeSpace)
+            
+            allocator.free(allocation: c)
+            allocator.free(allocation: d)
+            allocator.free(allocation: b)
+            allocator.free(allocation: e)
+            
+            let validateAll = allocator.allocate(size: 1024 * 1024 * 256)
+            XCTAssertEqual(validateAll.offset, 0)
+            allocator.free(allocation: validateAll)
+        }
+        
+        func testZeroFragmentation() {
+            var allocations = [Allocation](repeating: Allocation(), count: 256)
+            for i in 0..<256 {
+                allocations[i] = allocator.allocate(size: 1024 * 1024)
+                XCTAssertEqual(allocations[i].offset, UInt32(i) * 1024 * 1024)
+            }
+            
+            var report = allocator.storageReport()
+            XCTAssertEqual(report.totalFreeSpace, 0)
+            XCTAssertEqual(report.largestFreeRegion, 0)
+            
+            allocator.free(allocation: allocations[243])
+            allocator.free(allocation: allocations[5])
+            allocator.free(allocation: allocations[123])
+            allocator.free(allocation: allocations[95])
+            
+            allocator.free(allocation: allocations[151])
+            allocator.free(allocation: allocations[152])
+            allocator.free(allocation: allocations[153])
+            allocator.free(allocation: allocations[154])
+            
+            allocations[243] = allocator.allocate(size: 1024 * 1024)
+            allocations[5] = allocator.allocate(size: 1024 * 1024)
+            allocations[123] = allocator.allocate(size: 1024 * 1024)
+            allocations[95] = allocator.allocate(size: 1024 * 1024)
+            allocations[151] = allocator.allocate(size: 1024 * 1024 * 4)
+            XCTAssertNotEqual(allocations[243].offset, Allocation.NO_SPACE)
+            XCTAssertNotEqual(allocations[5].offset, Allocation.NO_SPACE)
+            XCTAssertNotEqual(allocations[123].offset, Allocation.NO_SPACE)
+            XCTAssertNotEqual(allocations[95].offset, Allocation.NO_SPACE)
+            XCTAssertNotEqual(allocations[151].offset, Allocation.NO_SPACE)
+            
+            for i in 0..<256 {
+                if i < 152 || i > 154 {
+                    allocator.free(allocation: allocations[i])
+                }
+            }
+            
+            report = allocator.storageReport()
+            XCTAssertEqual(report.totalFreeSpace, 1024 * 1024 * 256)
+            XCTAssertEqual(report.largestFreeRegion, 1024 * 1024 * 256)
+            
+            let validateAll = allocator.allocate(size: 1024 * 1024 * 256)
+            XCTAssertEqual(validateAll.offset, 0)
+            allocator.free(allocation: validateAll)
+        }
+        
+        testSimple()
+        testMergeTrivial()
+        testReuseTrivial()
+        testReuseComplex()
+        testZeroFragmentation()
     }
 }
